f19775d39b062162895b47e762e980a3f2bdcf03
unknown <mittervan@gmail.com>
2021-08-08 09:25:31 +0700

    Behivial Design Pattern

---
+++ src/Obsever/Observer.java
@@ -0,0 +1,6 @@
+package Obsever;
+
+abstract class Observer {
+    protected Subject subject;
+    public abstract void update();
+}
---
+++ src/Strategy/Main.java
@@ -0,0 +1,21 @@
+package Strategy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SortedList sortedList = new SortedList();
+        sortedList.add("Java");
+        sortedList.add("PHP");
+        sortedList.add("C#");
+        sortedList.add("Python");
+
+        sortedList.setSortStrategy(new QuickSort());
+        sortedList.sort();
+
+        sortedList.setSortStrategy(new MergeSort());
+        sortedList.sort();
+
+        sortedList.setSortStrategy(new SelectionSort());
+        sortedList.sort();
+    }
+}
